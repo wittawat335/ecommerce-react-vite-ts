b62e6fcaab62d27c5d5b230b66ce716432269482
Wittawat Fuangfu <wittawat335@gmail.com>
2023-12-27 14:42:00 +0700

    #

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import * as path from "path"; //npm i -D @types/node
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: { port: 3000 },
   plugins: [react()],
-})
+  resolve: {
+    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
+  },
+});
